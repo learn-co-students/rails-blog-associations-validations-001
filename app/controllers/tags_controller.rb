77ceab5a494cee7f3a15fa07f57e689beec2a7e2
Peter Jablonski <pwjablonski@gmail.com>
2015-09-10 01:45:39 +0000

    Done.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -29,9 +29,9 @@ class TagsController < ApplicationController
     respond_to do |format|
       if @tag.save
         format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
-        format.json { render :show, status: :created, location: @tag }
+        format.json { render action: 'show', status: :created, location: @tag }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @tag.errors, status: :unprocessable_entity }
       end
     end
@@ -43,9 +43,9 @@ class TagsController < ApplicationController
     respond_to do |format|
       if @tag.update(tag_params)
         format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tag }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @tag.errors, status: :unprocessable_entity }
       end
     end
@@ -56,7 +56,7 @@ class TagsController < ApplicationController
   def destroy
     @tag.destroy
     respond_to do |format|
-      format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
+      format.html { redirect_to tags_url }
       format.json { head :no_content }
     end
   end
