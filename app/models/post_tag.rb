2dba7745e96a712b6cac7aaec68cdb86361d2f00
epang1 <epang1@gmail.com>
2017-04-12 21:21:31 -0400

    done

---
+++ app/models/post_tag.rb
@@ -0,0 +1,7 @@
+class PostTag < ActiveRecord::Base
+
+  belongs_to :tag
+  belongs_to :post
+
+  
+end
---
+++ db/migrate/20170412193917_add_ato_post_tag.rb
@@ -0,0 +1,8 @@
+class AddAtoPostTag < ActiveRecord::Migration
+  def change
+    create_table :post_tags do |t|
+
+      t.timestamps null: false
+    end
+  end
+end
